401231564dcc1d0731158d5f29348d31ff016d7a
bashar <bashar.nofal1997@gmail.com>
2020-12-16 12:21:39 +0200

    added JS to website2

---
+++ website3/JavaScript.js
@@ -0,0 +1,24 @@
+var name = prompt("enter your name");
+
+
+var pass = prompt("enter your password");
+
+ if (pass == 123){
+    
+     alert( "hello" + name);
+    
+ }else {
+     
+     alert("wrong password");
+     location.reload();
+}
+
+
+
+
+
+
+
+
+
+
